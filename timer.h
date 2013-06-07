2769c4978c22bb2b228a245780978d3d83ab7fad
Sanyam Kapoor <ksanyam18@yahoo.com>
2013-06-07 07:40:27 +0530

    Create timer.h

---
+++ timer.h
@@ -0,0 +1,36 @@
+/******************************************************
+*Class for calculating time with microsecond precision*
+******************************************************/
+
+#include <iostream>
+#include <chrono>
+#include <ctime>
+
+using namespace std;
+//New namespace set in C++11
+using namespace chrono;
+
+//Units of time
+enum timeUnit{
+SEC,
+MILLISEC,
+MICROSEC
+};
+
+class Timer{
+public:
+Timer();
+
+~Timer();
+
+void start();
+void stop();
+
+int getElapsedTime(timeUnit unit);
+time_t getFinishedTime();
+
+private:
+//Time point private variables
+time_point<system_clock> startPoint, endPoint;
+
+};
